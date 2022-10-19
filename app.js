909ef6dd81a359035bf6eebd5bcefb55ff5acf1a
Yash <74345713+yash244466666@users.noreply.github.com>
2022-10-19 14:58:59 +0600

    Add changes as instructed

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const hamMenu = document.getElementById('navmenu');
 
+/* I've to linter out those functions because,
+otherwise they would
+give the, "no-unused-vars" linter error message.
+I've searched for the solution online,
+and then this is the solution I found. */
+
 /* eslint-disable */
 function hamMenuShow() {
   hamMenu.classList.remove('hidden');
