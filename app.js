8fc5980ba8354553594fa541b7c7b13c66f83075
Yash <74345713+yash244466666@users.noreply.github.com>
2022-10-19 08:38:39 +0600

    Add JS functionality for mobile menu

---
+++ app.js
@@ -0,0 +1,12 @@
+const hamMenu = document.getElementById('navmenu');
+
+/* eslint-disable */
+function hamMenuShow() {
+  hamMenu.classList.remove('hidden');
+}
+
+/* eslint-disable */
+function hamMenuHide() {
+  hamMenu.classList.add('hidden');
+}
+
